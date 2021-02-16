d9144f9a1f1cf86fea7d0385d23bd7081a9be41a
FerxhNova <ferxh77@gmail.com>
2021-02-16 15:04:54 -0600

    Add files via upload

---
+++ data.js
@@ -0,0 +1,154 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1conv",
+      "name": "1conv",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 500,
+      "initialViewParameters": {
+        "yaw": 0.7797205578231612,
+        "pitch": 0.22379723674728957,
+        "fov": 1.3514664571196955
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7004163309778342,
+          "pitch": 0.14613241628230433,
+          "rotation": 0,
+          "target": "1-2conv"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-2conv",
+      "name": "2conv",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 500,
+      "initialViewParameters": {
+        "yaw": -2.2588165838829166,
+        "pitch": 0.1166069475103999,
+        "fov": 1.3514664571196955
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.4652601497918667,
+          "pitch": 0.3842046482825445,
+          "rotation": 0,
+          "target": "2-3conv"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-3conv",
+      "name": "3conv",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 500,
+      "initialViewParameters": {
+        "yaw": 0.848360056772238,
+        "pitch": 0.11149470339783996,
+        "fov": 1.3514664571196955
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.48717349151534073,
+          "pitch": 0.4422230640386342,
+          "rotation": 0,
+          "target": "3-4conv"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-4conv",
+      "name": "4conv",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 500,
+      "initialViewParameters": {
+        "yaw": 0.8361533451278316,
+        "pitch": 0.38392900031552024,
+        "fov": 1.3514664571196955
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9309542910148281,
+          "pitch": 0.5858181413187022,
+          "rotation": 0,
+          "target": "4-5conv"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-5conv",
+      "name": "5conv",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 500,
+      "initialViewParameters": {
+        "yaw": -2.0599411269495906,
+        "pitch": -0.0011202449560201444,
+        "fov": 1.3514664571196955
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
